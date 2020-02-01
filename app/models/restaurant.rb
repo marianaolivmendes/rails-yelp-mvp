fe7177ddef254dd30846e181127116c1f1931b25
Mariana Mendes <mariana.omendes@gmail.com>
2020-02-01 15:21:57 +0000

    logic working correctly, front end not done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
-  validates :name, :address, :category, presence: true
-  validates :category, inclusion: { in: %w(chinese italian japonese french belgian)}
   has_many :reviews, dependent: :destroy
+  validates :address, :category, presence: true
+  validates :name, uniqueness: true, presence: true
+  validates :category, inclusion: { in: %w(Chinese Italian Japanese French Belgian)}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Bocca di Lupo',
+    address:      '12 Archer St',
+    category:     'Italian',
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'Italian',
+  },
+ {
+    name:         'Sushi Samba',
+    address:      'London Heron Tower',
+    category:     'Japanese',
+  },
+  {
+    name:         'La Petite Ferme',
+    address:      '104 Farringdon Rd',
+    category:     'French',
+  },
+  {
+    name:         'Peking Seoul London',
+    address:      '113-115 Praed St, Tyburnia',
+    category:     'Chinese',
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
